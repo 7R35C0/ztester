aee2db7d5402413deba2d5b44189407fc5446b8b
7R35C0 <7R35C0@proton.me>
2024-11-18 14:50:59 +0200

    update 4 files and create 3 files

--- build.zig
+++ build.zig
@@ -1,37 +1,274 @@
+//:========================================================================
+//: Licensed under the [MIT License](LICENSE).
+//:
+//: Tested with zig version 0.13.0 on Linux Fedora 39.
+//:
+//#
+//# WARNING
+//#   Be very careful with `rmv` step, the `setupRemove` does not check
+//#   arguments, it just silently removes any user provided paths and can
+//#   lead to unwanted results.
+//#
+//:
+//: IMPORTANT
+//: * Step `cov` assumes that [kcov](https://github.com/SimonKagstrom/kcov)
+//:   is installed.
+//: * Use a live http server to see the code coverage report and docs
+//:   (zig-out/cov/index.html, zig-out/doc/index.html).
+//: * Steps `run`, `fmt` and `rmv` can be used with arguments, like this
+//:     `zig build <step> -- arg1 arg2 ...`
+//:   where arguments are relative paths to the project folder.
+//:     eg. `zig build rmv -- zig-out/cov zig-out/doc/index.html`
+//: * Without arguments, step:
+//:   * `run` does nothing, it must be used with file paths
+//:   * `fmt` formats the files and folders defined in `setupFormat()`
+//:   * `rmv` removes the zig-cache and zig-out folders
+//:========================================================================
+
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+// general configuration
+const Config = struct {
+    name: []const u8,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+    root_source_file: std.Build.LazyPath,
+    version: std.SemanticVersion,
+};
 
-    const exe = b.addExecutable(.{
-        .name = "ztester",
-        .root_source_file = b.path("src/ztester.zig"),
-        .target = target,
-        .optimize = optimize,
+pub fn build(b: *std.Build) void {
+    // `name` is the name of project's root and entry file
+    const name = std.fs.path.basename(b.build_root.path.?);
+
+    // build configuration
+    const cfg = Config{
+        .name = name,
+        .target = b.standardTargetOptions(.{}),
+        .optimize = b.standardOptimizeOption(.{}),
+        .root_source_file = b.path(
+            b.fmt("src/{s}.zig", .{name}),
+        ),
+        .version = .{
+            .major = 0,
+            .minor = 1,
+            .patch = 0,
+        },
+    };
+
+    // expose library module for later use with `@import("cfg.name")`
+    const mod = setupModule(b, cfg);
+
+    // build static library
+    // command: zig build lib
+    // outputs: zig-out/lib
+    const lib = setupStaticLibrary(b, cfg);
+
+    // run tests
+    // command: zig build tst
+    // outputs: none
+    const tst = setupTest(b, cfg);
+
+    // generate code coverage
+    // command: zig build cov
+    // outputs: zig-out/cov
+    setupCoverage(b, tst);
+
+    // generate documentation
+    // command: zig build doc
+    // outputs: zig-out/doc
+    setupDocumentation(b, lib);
+
+    // run specific files
+    // command: zig build run -- path1 path2 ...
+    // outputs: zig-out/bin
+    setupRun(b, cfg, mod);
+
+    // format specific files and folders
+    // command: zig build fmt -- path1 path2 ...
+    // outputs: none
+    setupFormat(b);
+
+    // remove specific files and folders
+    // command: zig build rmv -- path1 path2 ...
+    // outputs: none
+    setupRemove(b);
+}
+
+fn setupModule(b: *std.Build, cfg: Config) *std.Build.Module {
+    return b.addModule(
+        cfg.name,
+        .{
+            .target = cfg.target,
+            .optimize = cfg.optimize,
+            .root_source_file = cfg.root_source_file,
+        },
+    );
+}
+
+fn setupStaticLibrary(b: *std.Build, cfg: Config) *std.Build.Step.Compile {
+    const lib_step = b.step(
+        "lib",
+        "Build static library",
+    );
+
+    const lib = b.addStaticLibrary(.{
+        .name = cfg.name,
+        .target = cfg.target,
+        .optimize = cfg.optimize,
+        .root_source_file = cfg.root_source_file,
+        .version = cfg.version,
     });
 
-    b.installArtifact(exe);
+    const lib_install = b.addInstallArtifact(
+        lib,
+        .{},
+    );
+    lib_step.dependOn(&lib_install.step);
 
-    const run_cmd = b.addRunArtifact(exe);
+    return lib;
+}
 
-    run_cmd.step.dependOn(b.getInstallStep());
+fn setupTest(b: *std.Build, cfg: Config) *std.Build.Step.Compile {
+    const tst_step = b.step(
+        "tst",
+        "Run tests",
+    );
+
+    const tst = b.addTest(.{
+        .name = cfg.name,
+        .target = cfg.target,
+        .optimize = cfg.optimize,
+        .root_source_file = cfg.root_source_file,
+        .version = cfg.version,
+    });
+
+    const tst_run = b.addRunArtifact(tst);
+    tst_step.dependOn(&tst_run.step);
+
+    return tst;
+}
+
+fn setupCoverage(b: *std.Build, tst: *std.Build.Step.Compile) void {
+    const cov_step = b.step(
+        "cov",
+        "Generate code coverage",
+    );
+
+    const cov_cache = b.pathJoin(&[_][]const u8{ b.cache_root.path.?, "cov" });
+
+    const cov_run = b.addSystemCommand(&.{
+        "kcov",
+        "--clean",
+        "--include-pattern=src/",
+        "--output-interval=0",
+        cov_cache,
+    });
+    cov_run.addArtifactArg(tst);
+
+    const cov_install = b.addInstallDirectory(.{
+        .install_dir = .{ .custom = "cov" },
+        .install_subdir = "",
+        .source_dir = .{
+            .src_path = .{
+                .owner = b,
+                .sub_path = ".zig-cache/cov",
+            },
+        },
+    });
+    cov_install.step.dependOn(&cov_run.step);
+
+    const cov_cache_remove = b.addRemoveDirTree(cov_cache);
+    cov_cache_remove.step.dependOn(&cov_install.step);
+    cov_step.dependOn(&cov_cache_remove.step);
+}
+
+fn setupDocumentation(b: *std.Build, lib: *std.Build.Step.Compile) void {
+    const doc_step = b.step(
+        "doc",
+        "Generate documentation",
+    );
+
+    const doc_install = b.addInstallDirectory(.{
+        .install_dir = .prefix,
+        .install_subdir = "doc",
+        .source_dir = lib.getEmittedDocs(),
+    });
+    doc_step.dependOn(&doc_install.step);
+}
+
+fn setupRun(b: *std.Build, cfg: Config, mod: *std.Build.Module) void {
+    const run_step = b.step(
+        "run",
+        "Run specific files",
+    );
+
+    if (b.args) |paths| {
+        for (paths) |path| {
+            const exe = b.addExecutable(.{
+                .name = std.fs.path.stem(path),
+                .target = cfg.target,
+                .optimize = cfg.optimize,
+                .root_source_file = .{
+                    .src_path = .{
+                        .owner = b,
+                        .sub_path = path,
+                    },
+                },
+                .version = cfg.version,
+            });
+            exe.root_module.addImport(cfg.name, mod);
+
+            const exe_install = b.addInstallArtifact(
+                exe,
+                .{},
+            );
+            const exe_run = b.addRunArtifact(exe);
+            exe_run.step.dependOn(&exe_install.step);
+            run_step.dependOn(&exe_run.step);
+        }
+    }
+}
+
+fn setupFormat(b: *std.Build) void {
+    const fmt_step = b.step(
+        "fmt",
+        "Format specific files and folders",
+    );
+
+    var paths: []const []const u8 = &.{};
 
     if (b.args) |args| {
-        run_cmd.addArgs(args);
+        paths = args;
+    } else {
+        paths = &.{
+            "bench",
+            "demo",
+            "src",
+            "utils",
+            "build.zig",
+            "build.zig.zon",
+        };
     }
 
-    const run_step = b.step("run", "Run application");
-    run_step.dependOn(&run_cmd.step);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/ztester.zig"),
-        .target = target,
-        .optimize = optimize,
+    const fmt = b.addFmt(.{
+        .paths = paths,
+        .check = false,
     });
+    fmt_step.dependOn(&fmt.step);
+}
 
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+fn setupRemove(b: *std.Build) void {
+    const rmv_step = b.step(
+        "rmv",
+        "Remove specific files and folders",
+    );
 
-    const test_step = b.step("test", "Test application");
-    test_step.dependOn(&run_exe_unit_tests.step);
+    if (b.args) |paths| {
+        for (paths) |path| {
+            rmv_step.dependOn(&b.addRemoveDirTree(path).step);
+        }
+    } else {
+        rmv_step.dependOn(&b.addRemoveDirTree(b.cache_root.path.?).step);
+        rmv_step.dependOn(&b.addRemoveDirTree(b.install_path).step);
+    }
 }
--- src/ztester.zig
+++ src/ztester.zig
@@ -1,329 +1,39 @@
+//:========================================================================
+//: Licensed under the [MIT License](LICENSE).
+//:
+//: Tested with zig version 0.13.0 on Linux Fedora 39.
+//:========================================================================
+
 const std = @import("std");
-const maxInteger = std.math.maxInt;
-const maxFloat = std.math.floatMax;
+const print = std.debug.print;
 
 pub fn main() void {
-    _ = countDigitSwitcher(0);
-    _ = countDigitSwitcher(0.0);
-
-    _ = countDigitSwitcher(1_000_000);
-    _ = countDigitSwitcher(-1_000_000);
-
-    _ = countDigitSwitcher(1_000_000.0);
-    _ = countDigitSwitcher(-1_000_000.0);
-
-    _ = countDigitSwitcher(@as(u32, 0));
-    _ = countDigitSwitcher(@as(f32, 0.0));
-
-    _ = countDigitSwitcher(@as(i32, 1_000_000));
-    _ = countDigitSwitcher(@as(i32, -1_000_000));
-
-    _ = countDigitSwitcher(@as(f32, 1_000_000.0));
-    _ = countDigitSwitcher(@as(f32, -1_000_000.0));
-
-    _ = countDigitSwitcher(@as(f16, 1_000.0));
-    _ = countDigitSwitcher(@as(f16, -1_000.0));
-
-    // // compile errors
-    // _ = countDigitSwitcher(340_282_366_920_938_463_463_374_607_431_768_211_455_000);
-    // _ = countDigitSwitcher(-340_282_366_920_938_463_463_374_607_431_768_211_455_000);
-    // _ = countDigitSwitcher(340_282_366_920_938_463_463_374_607_431_768_211_455.0);
-    // _ = countDigitSwitcher(-340_282_366_920_938_463_463_374_607_431_768_211_455.0);
-    // _ = countDigitSwitcher(@as(u256, 1_000_000));
-    // _ = countDigitSwitcher(@as(i256, -1_000_000));
-    // _ = countDigitSwitcher(@as(f64, 1_000_000.0));
-    // _ = countDigitSwitcher(@as(f128, -1_000_000.0));
-    // _ = countDigitSwitcher("1_000_000");
-    // _ = countDigitSwitcher(@as([]const u8, "-1_000_000.0"));
+    print("ztezter works!\n", .{});
 }
 
-pub fn countDigitSwitcher(number: anytype) usize {
-    const range_number: [40]u128 = .{
-        0,
-        10,
-        100,
-        1_000,
-        10_000,
-        100_000,
-        1_000_000,
-        10_000_000,
-        100_000_000,
-        1_000_000_000,
-        10_000_000_000,
-        100_000_000_000,
-        1_000_000_000_000,
-        10_000_000_000_000,
-        100_000_000_000_000,
-        1_000_000_000_000_000,
-        10_000_000_000_000_000,
-        100_000_000_000_000_000,
-        1_000_000_000_000_000_000,
-        10_000_000_000_000_000_000,
-        100_000_000_000_000_000_000,
-        1_000_000_000_000_000_000_000,
-        10_000_000_000_000_000_000_000,
-        100_000_000_000_000_000_000_000,
-        1_000_000_000_000_000_000_000_000,
-        10_000_000_000_000_000_000_000_000,
-        100_000_000_000_000_000_000_000_000,
-        1_000_000_000_000_000_000_000_000_000,
-        10_000_000_000_000_000_000_000_000_000,
-        100_000_000_000_000_000_000_000_000_000,
-        1_000_000_000_000_000_000_000_000_000_000,
-        10_000_000_000_000_000_000_000_000_000_000,
-        100_000_000_000_000_000_000_000_000_000_000,
-        1_000_000_000_000_000_000_000_000_000_000_000,
-        10_000_000_000_000_000_000_000_000_000_000_000,
-        100_000_000_000_000_000_000_000_000_000_000_000,
-        1_000_000_000_000_000_000_000_000_000_000_000_000,
-        10_000_000_000_000_000_000_000_000_000_000_000_000,
-        100_000_000_000_000_000_000_000_000_000_000_000_000,
-        340_282_366_920_938_463_463_374_607_431_768_211_455,
-    };
+//+========================================================================
+//+ Tests
+//+========================================================================
 
-    switch (@typeInfo(@TypeOf(number))) {
-        .Int, .ComptimeInt => {
-            if (@bitSizeOf(@TypeOf(number)) > @bitSizeOf(u128) or (@bitSizeOf(@TypeOf(number)) == 0 and @abs(number) > maxInteger(u128))) {
-                @compileError("unsupported type: " ++ @typeName(@TypeOf(number)) ++ " - countDigitSwitcher() is not implemented for a comptime_int greater than 128 bits");
-            }
-            for (range_number, 0..) |value, index| {
-                if (@abs(number) < value or (@abs(number) == value and index == range_number.len - 1)) return index;
-            }
-        },
-        .Float, .ComptimeFloat => {
-            if (@bitSizeOf(@TypeOf(number)) > @bitSizeOf(f32) or (@bitSizeOf(@TypeOf(number)) == 0 and @abs(number) > maxFloat(f32))) {
-                @compileError("unsupported type: " ++ @typeName(@TypeOf(number)) ++ " - countDigitSwitcher() is not implemented for a comptime_float greater than 32 bits");
-            }
+const expect = std.testing.expect;
 
-            for (range_number, 0..) |value, index| {
-                if (@trunc(@abs(number)) < @as(f32, @floatFromInt(value))) return index;
-            }
-        },
-        else => @compileError("unsupported type: " ++ @typeName(@TypeOf(number)) ++ " - countDigitSwitcher() is not implemented for " ++ @typeName(@TypeOf(number))),
-    }
+//^--------------------------------------------------------------
+//^ Just a dummy test
+//^--------------------------------------------------------------
 
-    return 0;
+test "ztester() - dummy" {
 
-    // return error.UnreachableCode;
-    // unreachable;
-    // @compileError("unreachable code");
-}
-
-//+===========================================================================================+
-//+                                          Tests                                            +
-//+===========================================================================================+
-
-test "countDigitSwitcher()" {
-    const minInteger = std.math.minInt;
-    const minFloat = std.math.floatMin;
-    const eqlTest = std.testing.expectEqual;
-
-    const result = countDigitSwitcher;
-
-    try eqlTest(1, result(0));
-    try eqlTest(1, result(minInteger(u8)));
-    try eqlTest(3, result(maxInteger(u8)));
-    for (0..3) |index| {
-        try eqlTest(index + 1, result(@as(u8, @intCast(std.math.pow(u128, 10, index)))));
-    }
-
-    try eqlTest(1, result(0));
-    try eqlTest(1, result(minInteger(u16)));
-    try eqlTest(5, result(maxInteger(u16)));
-    for (0..5) |index| {
-        try eqlTest(index + 1, result(@as(u16, @intCast(std.math.pow(u128, 10, index)))));
-    }
-
-    try eqlTest(1, result(0));
-    try eqlTest(1, result(minInteger(u32)));
-    try eqlTest(10, result(maxInteger(u32)));
-    for (0..10) |index| {
-        try eqlTest(index + 1, result(@as(u32, @intCast(std.math.pow(u128, 10, index)))));
-    }
-
-    try eqlTest(1, result(0));
-    try eqlTest(1, result(minInteger(u64)));
-    try eqlTest(20, result(maxInteger(u64)));
-    for (0..20) |index| {
-        try eqlTest(index + 1, result(@as(u64, @intCast(std.math.pow(u128, 10, index)))));
-    }
-
-    try eqlTest(1, result(0));
-    try eqlTest(1, result(minInteger(u128)));
-    try eqlTest(39, result(maxInteger(u128)));
-    for (0..39) |index| {
-        try eqlTest(index + 1, result(@as(u128, @intCast(std.math.pow(u128, 10, index)))));
-    }
-
-    try eqlTest(1, result(0));
-    try eqlTest(3, result(minInteger(i8)));
-    try eqlTest(3, result(maxInteger(i8)));
-    for (0..3) |index| {
-        try eqlTest(index + 1, result(0 - @as(i8, @intCast(std.math.pow(u128, 10, index)))));
-    }
-    for (0..3) |index| {
-        try eqlTest(index + 1, result(@as(i8, @intCast(std.math.pow(u128, 10, index)))));
-    }
-
-    try eqlTest(1, result(0));
-    try eqlTest(5, result(minInteger(i16)));
-    try eqlTest(5, result(maxInteger(i16)));
-    for (0..5) |index| {
-        try eqlTest(index + 1, result(0 - @as(i16, @intCast(std.math.pow(u128, 10, index)))));
-    }
-    for (0..5) |index| {
-        try eqlTest(index + 1, result(@as(i16, @intCast(std.math.pow(u128, 10, index)))));
-    }
-
-    try eqlTest(1, result(0));
-    try eqlTest(10, result(minInteger(i32)));
-    try eqlTest(10, result(maxInteger(i32)));
-    for (0..10) |index| {
-        try eqlTest(index + 1, result(0 - @as(i32, @intCast(std.math.pow(u128, 10, index)))));
-    }
-    for (0..10) |index| {
-        try eqlTest(index + 1, result(@as(i32, @intCast(std.math.pow(u128, 10, index)))));
-    }
-
-    try eqlTest(1, result(0));
-    try eqlTest(19, result(minInteger(i64)));
-    try eqlTest(19, result(maxInteger(i64)));
-    for (0..19) |index| {
-        try eqlTest(index + 1, result(0 - @as(i64, @intCast(std.math.pow(u128, 10, index)))));
-    }
-    for (0..19) |index| {
-        try eqlTest(index + 1, result(@as(i64, @intCast(std.math.pow(u128, 10, index)))));
-    }
-
-    try eqlTest(1, result(0));
-    try eqlTest(39, result(minInteger(i128)));
-    try eqlTest(39, result(maxInteger(i128)));
-    for (0..39) |index| {
-        try eqlTest(index + 1, result(0 - @as(i128, @intCast(std.math.pow(u128, 10, index)))));
-    }
-    for (0..39) |index| {
-        try eqlTest(index + 1, result(@as(i128, @intCast(std.math.pow(u128, 10, index)))));
-    }
-
-    try eqlTest(1, result(0.0));
-    try eqlTest(1, result(minFloat(f16)));
-    try eqlTest(1, result(0.0 - minFloat(f16)));
-    try eqlTest(5, result(maxFloat(f16)));
-    try eqlTest(5, result(0.0 - maxFloat(f16)));
-    for (0..5) |index| {
-        try eqlTest(index + 1, result(0.0 - @as(f16, @floatFromInt(std.math.pow(u128, 10, index)))));
-    }
-    for (0..5) |index| {
-        try eqlTest(index + 1, result(@as(f16, @floatFromInt(std.math.pow(u128, 10, index)))));
-    }
-
-    try eqlTest(1, result(0.0));
-    try eqlTest(1, result(minFloat(f32)));
-    try eqlTest(1, result(0.0 - minFloat(f32)));
-    try eqlTest(39, result(maxFloat(f32)));
-    try eqlTest(39, result(0.0 - maxFloat(f32)));
-    for (0..39) |index| {
-        try eqlTest(index + 1, result(0.0 - @as(f32, @floatFromInt(std.math.pow(u128, 10, index)))));
-    }
-    for (0..39) |index| {
-        try eqlTest(index + 1, result(@as(f32, @floatFromInt(std.math.pow(u128, 10, index)))));
+    //+ Test dummy.
+    {
+        try expect(true);
     }
 }
 
-// for other architectures add a specific case
-test "countDigitSwitcher() - 32/64 bits usize" {
-    const builtin = @import("builtin");
-    const minInteger = std.math.minInt;
-    const eqlTest = std.testing.expectEqual;
+//^--------------------------------------------------------------
+//^ Above imports
+//^--------------------------------------------------------------
 
-    const result = countDigitSwitcher;
-
-    switch (builtin.target.ptrBitWidth()) {
-        32 => {
-            try eqlTest(1, result(0));
-            try eqlTest(1, result(minInteger(usize)));
-            try eqlTest(10, result(maxInteger(usize)));
-            for (0..10) |index| {
-                try eqlTest(index + 1, result(@as(usize, @intCast(std.math.pow(u128, 10, index)))));
-            }
-
-            try eqlTest(result(0), result(0));
-            try eqlTest(result(minInteger(u32)), result(minInteger(usize)));
-            try eqlTest(result(maxInteger(u32)), result(maxInteger(usize)));
-            for (0..10) |index| {
-                try eqlTest(result(@as(u32, @intCast(std.math.pow(u128, 10, index)))), result(@as(usize, @intCast(std.math.pow(u128, 10, index)))));
-            }
-        },
-        64 => {
-            try eqlTest(1, result(0));
-            try eqlTest(1, result(minInteger(usize)));
-            try eqlTest(20, result(maxInteger(usize)));
-            for (0..20) |index| {
-                try eqlTest(index + 1, result(@as(usize, @intCast(std.math.pow(u128, 10, index)))));
-            }
-
-            try eqlTest(result(0), result(0));
-            try eqlTest(result(minInteger(u64)), result(minInteger(usize)));
-            try eqlTest(result(maxInteger(u64)), result(maxInteger(usize)));
-            for (0..20) |index| {
-                try eqlTest(result(@as(u64, @intCast(std.math.pow(u128, 10, index)))), result(@as(usize, @intCast(std.math.pow(u128, 10, index)))));
-            }
-        },
-        else => return error.SkipZigTest,
-    }
-}
-
-// for other architectures add a specific case
-test "countDigitSwitcher() - 32/64 bits isize" {
-    const builtin = @import("builtin");
-    const minInteger = std.math.minInt;
-    const eqlTest = std.testing.expectEqual;
-
-    const result = countDigitSwitcher;
-
-    switch (builtin.target.ptrBitWidth()) {
-        32 => {
-            try eqlTest(1, result(0));
-            try eqlTest(10, result(minInteger(isize)));
-            try eqlTest(10, result(maxInteger(isize)));
-            for (0..10) |index| {
-                try eqlTest(index + 1, result(0 - @as(isize, @intCast(std.math.pow(u128, 10, index)))));
-            }
-            for (0..10) |index| {
-                try eqlTest(index + 1, result(@as(isize, @intCast(std.math.pow(u128, 10, index)))));
-            }
-
-            try eqlTest(result(0), result(0));
-            try eqlTest(result(minInteger(i32)), result(minInteger(isize)));
-            try eqlTest(result(maxInteger(i32)), result(maxInteger(isize)));
-            for (0..10) |index| {
-                try eqlTest(result(0 - @as(i32, @intCast(std.math.pow(u128, 10, index)))), result(0 - @as(isize, @intCast(std.math.pow(u128, 10, index)))));
-            }
-            for (0..10) |index| {
-                try eqlTest(result(@as(i32, @intCast(std.math.pow(u128, 10, index)))), result(@as(isize, @intCast(std.math.pow(u128, 10, index)))));
-            }
-        },
-        64 => {
-            try eqlTest(1, result(0));
-            try eqlTest(19, result(minInteger(isize)));
-            try eqlTest(19, result(maxInteger(isize)));
-            for (0..19) |index| {
-                try eqlTest(index + 1, result(0 - @as(isize, @intCast(std.math.pow(u128, 10, index)))));
-            }
-            for (0..19) |index| {
-                try eqlTest(index + 1, result(@as(isize, @intCast(std.math.pow(u128, 10, index)))));
-            }
-
-            try eqlTest(result(0), result(0));
-            try eqlTest(result(minInteger(i64)), result(minInteger(isize)));
-            try eqlTest(result(maxInteger(i64)), result(maxInteger(isize)));
-            for (0..19) |index| {
-                try eqlTest(result(0 - @as(i64, @intCast(std.math.pow(u128, 10, index)))), result(0 - @as(isize, @intCast(std.math.pow(u128, 10, index)))));
-            }
-            for (0..19) |index| {
-                try eqlTest(result(@as(i64, @intCast(std.math.pow(u128, 10, index)))), result(@as(isize, @intCast(std.math.pow(u128, 10, index)))));
-            }
-        },
-        else => return error.SkipZigTest,
-    }
+//+ Test all above imports.
+test "algore" {
+    std.testing.refAllDecls(@This());
 }
